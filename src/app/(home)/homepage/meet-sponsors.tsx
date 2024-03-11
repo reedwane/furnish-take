8c8f8f998423bf6bb4077bfcf6bd8629cc362b7d
reedwane <abdulkareemtemmy07@gmail.com>
2024-03-11 21:47:27 +0100

    add rifaaq

--- src/app/(home)/homepage/meet-sponsors.tsx
+++ src/app/(home)/homepage/meet-sponsors.tsx
@@ -5,18 +5,46 @@ import Image from "next/image";
 import logo from "/public/logo.png";
 
 const MeetSponsors = () => {
+  const sponsors = [
+    {
+      image: "/logo.png",
+      name: "",
+      url: "/",
+    },
+    {
+      image: "/partners/rifaaq-fav.png",
+      name: "Rifaaq",
+      url: "https://rifaaq.com",
+    },
+    {
+      image: "/logo.png",
+      name: "",
+      url: "/",
+    },
+  ];
   return (
     <Maxwidth tw="py-6 lg:(py-14 pt-20) flex flex-col">
       <FancyHeader primary="MEET OUR" secondary="SPONSORS & PARTNERS" />
 
       <ul tw="flex gap-7 overflow-x-auto pt-10 pb-3 md:(gap-10 pb-7) lg:(pt-16)">
-        {[...Array(5)].map((_, index) => (
+        {sponsors.map((sponsor, index) => (
           <li key={index}>
-            <Image
-              src={logo}
-              alt="partner"
-              tw="max-w-[58px] lg:(max-w-[132px])"
-            />
+            <a
+              href={sponsor.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              tw="flex flex-col items-center"
+            >
+              <div tw="relative w-[58px] h-[58px]  max-w-[58px] lg:(max-w-[132px])">
+                <Image
+                  src={sponsor.image}
+                  alt="partner"
+                  fill
+                  tw="object-contain"
+                />
+              </div>
+              {sponsor.name ? <p>{sponsor.name}</p> : null}
+            </a>
           </li>
         ))}
       </ul>
