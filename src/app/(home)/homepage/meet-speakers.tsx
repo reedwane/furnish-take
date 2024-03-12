89b89a2636d49cec5d91fa2f3404e31cbce9283f
Ibrahim Abdulhameed <i.ibrahim.abdulhameed@gmail.com>
2024-03-12 10:41:44 +0100

    update images

--- src/app/(home)/homepage/meet-speakers.tsx
+++ src/app/(home)/homepage/meet-speakers.tsx
@@ -4,16 +4,19 @@ import { Button, FancyHeader, Maxwidth } from "@/components";
 import PersonCard from "@/components/PersonCard";
 import { routePaths } from "@/utils/routepaths";
 import Link from "next/link";
+import { speakers } from "@/utils/People";
 
 const MeetSpeakers = () => {
   return (
     <Maxwidth tw="py-6 lg:py-14 flex flex-col">
       <FancyHeader primary="MEET OUR" secondary="SPEAKERS" />
 
+
+      
       <ul tw="flex gap-7 overflow-x-auto pt-10 pr-10 pb-3 md:(gap-10 pb-7) lg:(pt-16)">
-        {[...Array(5)].map((_, index) => (
-          <li key={index}>
-            <PersonCard tw="min-w-[300px] md:(min-w-[493px] max-h-[473px])" />
+        {speakers.map((person, index)=>(
+          <li key={index} >
+            <PersonCard person={person} />
           </li>
         ))}
       </ul>
